28f44974285e76dd66d92e0f0a4fb5267461922d
om <you@example.com>
2024-02-14 13:17:13 +0530

    Index

--- index.py
+++ index.py
@@ -9,5 +9,5 @@ app.include_router(Department,prefix='/Department')
 def Demo():
     return{"msg":"Welcome to Fast"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
