ebd4d80adf6b76d9f2ec8b1d288c1456084b238c
om <you@example.com>
2024-02-15 17:36:34 +0530

    s

--- utility/DBConfig.py
+++ utility/DBConfig.py
@@ -8,7 +8,7 @@ password='Garment'
 
 
 connection = (
-    f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=no')
+    f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;')
 
 # print("DB Connected")
 
