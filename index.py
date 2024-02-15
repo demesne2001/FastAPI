29c3537bb9033597487fa71b42aafcd73b7e6993
om <you@example.com>
2024-02-15 12:56:23 +0530

    v

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 from os import path
-from .Controller import mstDepartmentController
+from Controller import mstDepartmentController
 app=FastAPI()
 
-app.include_router(mstDepartmentController,prefix='/Department')
+app.include_router(mstDepartmentController.Department,prefix='/Department')
 
 @app.get("/")
 def Demo():
