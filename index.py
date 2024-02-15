f0be07257088dec8007ee7b402675db6b39f4669
om <you@example.com>
2024-02-15 14:49:06 +0530

    please

--- index.py
+++ index.py
@@ -10,9 +10,9 @@ app.include_router(mstDepartmentController.Department,prefix='/Department')
 def Demo():
     return{"msg":"Welcome to Fast"}
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 #     import sys
 #     sys.path.append(path.join(path.dirname(__file__),'..'))
     
 
-#     uvicorn.run(app, host="127.0.0.1", port=2023)
+     uvicorn.run(app, host="127.0.0.1", port=8000)
