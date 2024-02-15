fcfbf978b47b93899e178ae2e557de159e3b73a4
om <you@example.com>
2024-02-15 12:19:10 +0530

    check

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from os import path
 from Controller.mstDepartmentController import Department
 app=FastAPI()
 
@@ -10,4 +11,8 @@ def Demo():
     return{"msg":"Welcome to Fast"}
 
 # if __name__ == "__main__":
+#     import sys
+#     sys.path.append(path.join(path.dirname(__file__),'..'))
+    
+
 #     uvicorn.run(app, host="127.0.0.1", port=2023)
