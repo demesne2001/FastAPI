6704aa5ac190a43f91f7d1265677e13688511b72
om <you@example.com>
2024-02-17 14:07:05 +0530

    s

--- index.py
+++ index.py
@@ -2,9 +2,15 @@ import uvicorn
 from fastapi import FastAPI
 from os import path
 from Controller import mstDepartmentController
+from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
 
+
 app.include_router(mstDepartmentController.Department,prefix='/Department')
+
+origins=['*']
+
+app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=['*'],allow_headers=['*'],)
 
 @app.get("/")
 def Demo():
@@ -13,6 +19,4 @@ def Demo():
 if __name__ == "__main__":
 #     import sys
 #     sys.path.append(path.join(path.dirname(__file__),'..'))
-    
-
      uvicorn.run(app, host="127.0.0.1", port=8000)
