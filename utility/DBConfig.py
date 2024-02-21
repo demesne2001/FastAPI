eb6d98c3a8ffcaeb49f1e146aa9f083d2e1e074e
om <you@example.com>
2024-02-21 17:57:04 +0530

    check

--- utility/DBConfig.py
+++ utility/DBConfig.py
@@ -9,11 +9,11 @@ server1='192.168.2.252'
 database1='GRetail_Extreme'
 username1='Garment'
 password1='Garment'
-# connection = (
-#     f'DRIVER=ODBC Driver 18 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;Encrypt=no;Connection Timeout=30;')
-
 connection = (
-    f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server1};DATABASE={database1};UID={username1};PWD={password1};')
+    f'DRIVER=ODBC Driver 18 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;Encrypt=no;Connection Timeout=30;')
+
+# connection = (
+#     f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server1};DATABASE={database1};UID={username1};PWD={password1};')
 
 
 
