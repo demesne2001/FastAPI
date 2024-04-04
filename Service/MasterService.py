506fdf49bef4b7928bb84afc64d3e770ea824975
patelom0910 <patelom0910@gmail.com>
2024-04-04 14:39:23 +0530

    nn

--- Service/MasterService.py
+++ Service/MasterService.py
@@ -4,17 +4,20 @@ from Entity.Result import CommonListingResult
 
 def commanListingService(input:Listinginput,SpName:str,MethodName:str):
     result=CommonListingResult()
+    print('input',input)
     if(input.PageNo<=0):
         result.Message.append("PageNo Required Field")
     elif(input.Pagesize<=0):
         result.Message.append("Pagesize Required Field")
-    if(result.Message.count()<=0):
+    print(len(result.Message))
+    if(len(result.Message)==0):
         param=""
         if(input.PageNo>0):
             param +=f" @PageNo={input.PageNo},"
-        if(input.PageSize>0):
-            param +=f" @PageSize={input.PageSize},"
-        param +=f" @Search='{input.Search}'"     
+        if(input.Pagesize>0):
+            param +=f" @PageSize={input.Pagesize},"
+        param +=f" @Search='{input.Search}'"  
+        print(param)   
         result.lstResult=DBConfig.ExecuteDataReader(param,SpName,MethodName)
     else :
         result.HasError=True
