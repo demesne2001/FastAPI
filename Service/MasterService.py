c90217d4e44e962b2963d811370577d2ef286dbd
Abhay <python213@gmail.com>
2024-04-04 19:15:50 +0530

    new version

--- Service/MasterService.py
+++ Service/MasterService.py
@@ -22,4 +22,25 @@ def commanListingService(input:Listinginput,SpName:str,MethodName:str):
     else :
         result.HasError=True
     return result
+
+def commanDeleteService(input:Listinginput,SpName:str,MethodName:str):
+    result=CommonListingResult()
+    print('input',input)
+    if(input.PageNo<=0):
+        result.Message.append("PageNo Required Field")
+    elif(input.Pagesize<=0):
+        result.Message.append("Pagesize Required Field")
+    print(len(result.Message))
+    if(len(result.Message)==0):
+        param=""
+        if(input.PageNo>0):
+            param +=f" @PageNo={input.PageNo},"
+        if(input.Pagesize>0):
+            param +=f" @PageSize={input.Pagesize},"
+        param +=f" @Search='{input.Search}'"  
+        print(param)   
+        result.lstResult=DBConfig.ExecuteDataReader(param,SpName,MethodName)
+    else :
+        result.HasError=True
+    return result
     
